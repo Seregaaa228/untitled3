03dc4ac669c14925d5fb86a88581d46d211e2187
Seregaaa228 <75165284+Seregaaa228@users.noreply.github.com>
2021-02-18 12:54:45 +0200

    1

--- src/SafeCopyFile.java
+++ src/SafeCopyFile.java
@@ -1,53 +1,31 @@
 import java.io.*;
-import java.nio.channels.FileChannel;
 
 
 public class SafeCopyFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        File source = new File("C:\\Users\\Serega\\Desktop\\UserIo\\Valera.txt");
-        File dest = new File("C:\\Users\\Serega\\Desktop\\UserIo\\Valera22222222.txt");
-        FileInputStream fileInputStream = null;
-        FileOutputStream outputStream = null;
-
-
         try {
-
-            fileInputStream = new FileInputStream(source);
-        } catch (IOException e) {
-            System.out.println("Файл не существует.");
-        }
-        try {
-            fileInputStream = new FileInputStream(source);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Введите повторно ссылку");
-        try {
-            outputStream = new FileOutputStream(reader.readLine());
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-
-        while (true) {
-            try {
-                if (!(fileInputStream.available() > 0)) break;
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-
-            try {
-                outputStream.write(fileInputStream.read());
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-
-
+            System.out.println("Введите первую файл и второй");
+            createReader(reader.readLine(), reader.readLine());
+        } catch (FileNotFoundException exception) {
+            System.out.println("Ошибка");
+            System.out.println("Введите первый файл  и второй файл заново");
+            createReader(reader.readLine(), reader.readLine());
         }
     }
+    static public void createReader(String f1, String f2) throws IOException {
+        String str = "";
+        FileReader fr = new FileReader(f1);
+        BufferedReader br = new BufferedReader(fr);
+        FileWriter fw = new FileWriter(f2);
+        BufferedWriter bw = new BufferedWriter(fw);
+        while ((str = br.readLine()) != null) {
+            bw.write(str);
+            bw.newLine();
+            System.out.println("Скопировано");
+            bw.flush();
+        }
+        br.close();
+        bw.close();
+    }
 }
-
-
-
-
-
